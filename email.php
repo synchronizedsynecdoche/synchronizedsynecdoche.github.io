2eefe0a73bd84509b0d651b59c6c2560ca733854
csculley <cs_sculley@yahoo.com>
2017-10-18 00:12:28 -0400

    Attempt to get email

---
+++ email.php
@@ -0,0 +1,47 @@
+<?php
+// define variables and set to empty values
+$fnameErr = $lnameErr = $msgErr = "";
+$fname = $lname = $msg = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  if (empty($_POST["inputFirstName"])) {
+    $fnameErr = "First name is required";
+  } else {
+    $fname = test_input($_POST["inputFirstName"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$fname)) {
+      $fnameErr = "Only letters and white space allowed"; 
+    }
+  }
+
+  if (empty($_POST["inputLastName"])) {
+    $lnameErr = "Last name is required";
+  } else {
+    $lname = test_input($_POST["inputFirstName"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$lname)) {
+      $lnameErr = "Only letters and white space allowed"; 
+    }
+  }
+
+  if (empty($_POST["inputContact"])) {
+    $msg = "";
+  } else {
+    $msg = test_input($_POST["inputContact"]);
+  }
+
+    $msg = $fname . " " . $lname . "\n\n" . $msg;
+
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    mail("cs.sculley@gmail.com","NEGITOROMAKI",$msg);
+
+    echo "Mail sent, thanks " . $first_name . "!";
+}
+?>
