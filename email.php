135657d07c22e444f4a48763bfbf76c3c58cb643
csculley <cs_sculley@yahoo.com>
2017-10-23 00:30:01 -0400

    Actually fix email checking

--- email.php
+++ email.php
@@ -1,6 +1,6 @@
 <?php
 // define variables and set to empty values
-$fnameErr = $lnameErr = $msgErr = $emailErr = "";
+$fnameErr = $lnameErr = $emailErr = "";
 $fname = $lname = $msg = $email = "";
 
 function test_input($data) {
@@ -13,32 +13,32 @@ function test_input($data) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
   if (empty($_POST["inputFirstName"])) {
-    $fnameErr = "First name is required";
+    $fnameErr = "* First name is required";
   } else {
     $fname = test_input($_POST["inputFirstName"]);
     // check if name only contains letters and whitespace
     if (!preg_match("/^[a-zA-Z ]*$/",$fname)) {
-      $fnameErr = "Only letters and white space allowed"; 
+      $fnameErr = "* Only letters and white space allowed"; 
     }
   }
 
   if (empty($_POST["inputLastName"])) {
-    $lnameErr = "Last name is required";
+    $lnameErr = "* Last name is required";
   } else {
     $lname = test_input($_POST["inputLastName"]);
     // check if name only contains letters and whitespace
     if (!preg_match("/^[a-zA-Z ]*$/",$lname)) {
-      $lnameErr = "Only letters and white space allowed"; 
+      $lnameErr = "* Only letters and white space allowed"; 
     }
   }
 
   if (empty($_POST["inputEmail"])) {
-    $emailErr = "Email is required";
+    $emailErr = "* Email is required";
   } else {
     $email = test_input($_POST["inputEmail"]);
     // check if e-mail address is well-formed
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format"; 
+      $emailErr = "* Invalid email format"; 
     }
   }
 
@@ -48,7 +48,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $msg = test_input($_POST["inputContact"]);
   }
   
-	if (!empty($fname) or !empty($lname) or !empty($email)) {
+	if (!empty($fnameErr) or !empty($lnameErr) or !empty($emailErr)) {
 		$msg = "Mail from: " . $fname . " " . $lname . " at " . $email . ".\n\n" . $msg;
 		mail("cs.sculley@gmail.com","NEGITOROMAKI",$msg);
 		echo "Mail sent, thanks " . $fname . "!";
